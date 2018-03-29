2a8d13d687837afb9e5670e251e7724bdcc5f910
VovaPylypenko <Pylypenko199813@gmail.com>
2018-03-29 16:04:03 +0300

    send message

--- src/main/java/core/Bot.java
+++ src/main/java/core/Bot.java
@@ -1,7 +1,10 @@
 package core;
 
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import java.util.List;
 
@@ -10,11 +13,34 @@ public class Bot extends TelegramLongPollingBot{
     private static final String BOT_NAME = "everydayhelpbot";
     private static final String BOT_TOKEN = "427144383:AAEtk7MbtsIDc2ea9TiiPzJGCyvEzhxaOCc";
 
-    public void onUpdateReceived(Update update) {
+    public Bot(){
 
     }
 
-    public void onUpdatesReceived(List<Update> updates) {
+    public void onUpdateReceived(Update update) {
+
+        Message message = update.getMessage();
+
+        if(message.hasText()){
+            if(message.getText().equals("/start")){
+
+                String msg = "Hello," + message.getChat().getFirstName() + "!" +
+                        "\nI can help you in everyday life." +
+                        "\nNow I can:" +
+                        "\nSay hello.";
+
+                SendMessage sendMsg = new SendMessage()
+                        .setChatId(message.getChatId().toString())
+                        .setText(msg);
+
+                try {
+                    execute(sendMsg);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
 
     }
 
